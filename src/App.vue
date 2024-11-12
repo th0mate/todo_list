38b3111dde85b70fed2428ce6d57854cbb3934c2
loyet <thomas.loye@etu.umontpellier.fr>
2024-11-12 10:00:19 +0100

    feat : exos 1 à 7 ok (intro vue)

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref} from 'vue';
+
+const texte = "un texte qui s'affiche";
+const urlVue = "https://vuejs.org/";
+
+const compteur = ref(7);
+
+function incremente() {
+  compteur.value++;
+}
 </script>
 
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="wrapper">
+    <h1>TODO liste</h1>
+    <p>{{ texte }}</p>
+    <a v-bind:href="urlVue">Le site de vue</a>
+    <button v-on:click="incremente">+</button>
+    <p>{{ compteur }}</p>
+    <input v-model="compteur">
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
